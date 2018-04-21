97aed3f96d62bf7cf851eca7166be851a67c3840
uzulove <uyio2002@gmail.com>
2018-04-21 14:09:07 +0900

    init ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'pm2_deploy',
+      script    : 'index.js',
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : '54.64.28.43',
+      ref  : 'origin/master',
+      repo : 'git@github.com:CheolHoSin/pm_deploy.git',
+      path : '/home/ubuntu/nodejs/helloworld',
+      'post-deploy' : 'pm2 reload ecosystem.config.js'
+    },
+  }
+};
